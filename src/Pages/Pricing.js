06b606f0dcbff37836ade20e09576efb74c9c495
Muhammad <mwasif66625426@gmail.com>
2025-08-20 15:31:41 +0500

    done

--- src/Pages/Pricing.js
+++ src/Pages/Pricing.js
@@ -312,41 +312,6 @@ const PricingCard = () => {
     <>
       <div className={styles.pricing_container}>
         <div className={styles.pricing_header}>
-          <h1>Pricing</h1>
-          <p>All-inclusive plans. No contract. Cancel anytime.</p>
-          <div className={styles.workBgWrapper}>
-            <img
-              src="https://cdn.prod.website-files.com/63a9cb71c629474d4ae334b9/647884ab1f140c6821c5fa09_Group%204823.svg"
-              loading="lazy"
-              alt=""
-              className={styles.work_dec_1}
-            />
-            <img
-              src="https://cdn.prod.website-files.com/63a9cb71c629474d4ae334b9/647884aba97aaae67c2a5ea2_Group%204822.svg"
-              loading="lazy"
-              alt=""
-              className={styles.work_dec_4}
-            />
-            <img
-              src="https://cdn.prod.website-files.com/63a9cb71c629474d4ae334b9/647884ab33e01215443f94d9_Group%204824.svg"
-              loading="lazy"
-              alt=""
-              className={styles.work_dec_2}
-            />
-            <img
-              src="https://cdn.prod.website-files.com/63a9cb71c629474d4ae334b9/64789a0a94d2fcfd0eb19717_Frame%2048311.svg"
-              loading="lazy"
-              alt=""
-              className={styles.work_dec_3}
-            />
-
-            <img
-              src="https://cdn.prod.website-files.com/63a9cb71c629474d4ae334b9/647884ab33593e56fc75b54e_Ellipse%20164.svg"
-              loading="lazy"
-              alt=""
-              className={styles.work_dec_5}
-            />
-          </div>
           <div className={styles.billing_cycle_selector}>
             <div className={styles.main_filter}>
               <button
--- src/Pages/Pricings.js
+++ src/Pages/Pricings.js
@@ -4,11 +4,13 @@ import WorkGrid from "../Components/WorkGrid";
 import ReviewsSection from "../Components/Reviews";
 import FAQ from "../Components/FAQ";
 import PricingCard from "./Pricing";
+import PricingHeader from "./PricingHeader";
 
 const Pricings = () => {
   return (
     <>
       <div className="container">
+        <PricingHeader />
         <PricingCard />
         <Plans />
         <WorkGrid />
